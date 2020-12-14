795afad88b929d68bba2b1aee41d129bda266c6a
teriyakisource <vassilisnin@gmail.com>
2020-12-14 22:51:07 +0000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,8 @@
+echo "running setup"
+sudo apt update
+sudo apt install wget
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt -y install nodejs
+sudo apt -y  install gcc g++ make
+sudo apt install npm
